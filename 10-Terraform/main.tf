fd69eb3e6476cc02e4a61a1cd293794b9253a247
Temi Makinde <Temi.Makinde@Gmail.com>
2023-03-28 16:03:03 +0100

    using provisioners in terraform is the last resort and not encouraged

--- 10-Terraform/main.tf
+++ 10-Terraform/main.tf
@@ -21,6 +21,8 @@ variable "my_ip" {}
 
 variable "instance_type" {}
 
+variable "private_key_location" {}
+
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
@@ -137,7 +139,24 @@ resource "aws_instance" "myapp-server" {
   associate_public_ip_address = true
   key_name                    = aws_key_pair.ssh-key.key_name
 
-  user_data = file("entry-script.sh")
+  // user_data = file("entry-script.sh")
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.private_key_location)
+  }
+
+  provisioner "file" {
+    source      = "entry-script.sh"
+    destination = "/home/ec2-user/entry-script-on-ec2.sh"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "export ENV=dev",
+      "mkdir newdir"
+    ]
+  }
   tags = {
     Name = "${var.env_prefix}-server"
   }
