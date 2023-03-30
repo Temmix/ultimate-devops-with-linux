cc70116e3c67d90c8c86cda2ee319d1d9b16594a
Temi Makinde <Temi.Makinde@Gmail.com>
2023-03-30 23:35:43 +0100

    on going

---
+++ 11-python-automation/eks-status.py
@@ -0,0 +1,15 @@
+import boto3
+
+client = boto3.client('eks', region_name="eu-west-3")
+clusterNames = client.list_clusters()['clusters']
+
+for clusterName in clusterNames:
+    response = client.describe_cluster(name=clusterName)
+    cluster_info = response['cluster']
+    cluster_status = cluster_info['status']
+    cluster_endpoint = cluster_info['endpoint']
+    cluster_version = cluster_info['version']
+
+    print(f"Cluster {clusterName} status is {cluster_status}")
+    print(f"Cluster endpoint is : {cluster_endpoint}")
+print("###################################################")
